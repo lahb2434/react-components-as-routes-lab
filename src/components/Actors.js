e8c9a92c0950cef90276f61e309a306f9527b2c0
Christopher Houghton <ChrisHoughton@ChristopherMBP.fios-router.home>
2021-11-29 17:20:37 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { actors } from '../data';
 
+const actor = () => {
+  return actors.map(actor => 
+    <div>
+      <h4>{actor.name}</h4>
+      <label>movies:</label>
+      <ul>
+        {actor.movies.map(movie => <li>{movie}</li> )}
+      </ul>
+    </div>
+  )
+}
+
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actor()}
     </div>
   );
 };
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { directors } from '../data';
 
+const director = () => {
+  return directors.map(director => 
+    <div>
+      <h4>{director.name}</h4>
+      <label>movies:</label>
+      <ul>
+        {director.movies.map(movie => <li>{movie}</li> )}
+      </ul>
+    </div>
+  )
+}
+
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {director()}
     </div>
   );
 }
