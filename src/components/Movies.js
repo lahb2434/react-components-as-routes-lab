2099fde3aec3db3886435aaaf4b9f5be0d85994b
Christopher Houghton <ChrisHoughton@ChristopherMBP.fios-router.home>
2021-11-29 17:11:02 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { movies } from '../data';
 
+const movie = () => {
+  return movies.map(movie => 
+    <div>
+      <h4>{movie.title}</h4>
+      <p>{movie.time}</p>
+      <label>genres:</label>
+      <ul>
+        {movie.genres.map(genre => <li>{genre}</li> )}
+      </ul>
+    </div>
+  )
+}
+
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movie()}
     </div>
   );
 };
